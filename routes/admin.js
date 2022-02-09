70f1ed7c7bcc43380696cb3401cf3cb8e4851929
Mstars_Lab1_03 <mstars_lab1_03@Mstars-Lab1-03s-iMac.local>
2022-02-09 14:16:36 +0800

    [ADD] adding api

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,19 @@ const express = require("express");
 const path = require('path')
 const app = express();
 
+const {books} = require('../book.json')
+
+app.set("view engine" , "ejs")
+app.set("views" , path.join(__dirname , "../views"))
+app.set("view options" , {layout: false})
+
+app.use("/books" , (req , res)=>{
+    res.render("books" , {books})
+})
+
+
+app.get("/" , (req, res)=>{
+    res.render("index")
+})
+
 module.exports = app
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,87 @@
 const express = require("express");
+const { json } = require("express/lib/response");
+const res = require("express/lib/response");
+const fs = require("fs");
 const app = express();
+const router = express.Router();
+const { books } = require("../book.json");
 
-module.exports = app
+
+//Хэрэглэгч номын систем рүү нэврэх бүрт санамсаргүй байдлаар 3 номыг харах
+router.get("/random", (req, res) => {
+  let a = [];
+  var random = Math.floor(Math.random() * books.length);
+  for (i = 0; i < 3; i++) {
+    while (a.includes(random) > 0) {
+      random = Math.floor(Math.random() * books.length);
+    }
+    a.push(books[random]);
+  }
+  res.send(a);
+});
+
+// Хамгийн сүүлээс эхэн хүртэл хэвлэгдсэн дарааллаар номын мэдээллийг авна
+router.get("/sort", (req, res) => {
+    let dates = books.sort((a, b) => {
+        return new Date(b.published) - new Date(a.published); 
+      })
+    res.send(dates)
+});
+
+//номын сан дахь бүх зохиолчдын нэрийг авах
+router.get("/names" , (req ,res)=>{
+   let names = []
+   books.map((j)=>{names += j.author})
+   res.send(names)
+})
+
+//бүх номын мэдээллийг авах
+router.get("/books" , (req ,res)=>{
+    res.send(books)
+})
+
+//ISBN дугаараар номын мэдээлэл буцаах
+router.get("/book/:isbn" , (req ,res)=>{
+    let isbn_id = req.params.isbn
+    book = books.filter(book=>{
+        return book.isbn === isbn_id
+    })
+    res.send(book)
+})
+
+//номын нэрээр хайлт хийх
+router.get("/search" , (req ,res)=>{
+    title = req.query.title
+    result = books.filter(book=>{
+        return book.title.includes(title)
+    })
+    res.send(result)
+})
+
+//Хамгийн их хуудастай номын мэдээлэл авах
+router.get("/maxpages" , (req , res)=>{
+    pages = books.map((books)=>{return books.pages})
+    maxPages = Math.max(...pages)
+    maximumPages = books.filter(book=>{
+        return book.pages == maxPages
+    })
+    res.send(maximumPages)
+})
+
+//Хамгийн бага хуудастай номын мэдээлэл авах
+router.get("/minpages" , (req , res)=>{
+    pages = books.map((books)=>{return books.pages})
+    minPages = Math.min(...pages)
+    minimumPages = books.filter(book=>{
+        return book.pages == minPages
+    })
+    res.send(minimumPages)
+})
+
+//Хэвлэлийн компаниудыг жагсаан дор бүрнээ хэдэн ном бидэнд нийлүүлсэн талаарх мэдээлэл авах
+router.get("/company" , (req , res)=>{
+
+    res.send
+})
+
+module.exports = router;
